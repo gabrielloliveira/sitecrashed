b42da79715b49bb9a7d942d5e757a229ff6ca67c
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2020-06-29 19:47:51 -0300

    added tests and models for sites and events

--- sitecrashed/core/models.py
+++ sitecrashed/core/models.py
@@ -1,3 +1,30 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Site(models.Model):
+    uuid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.url
+
+
+class Event(models.Model):
+    UP = 'UP'
+    DOWN = 'DOWN'
+    NOTIFICATION = 'NOTIFICATION'
+    TYPE_CHOICES = (
+        (UP, 'UP'),
+        (DOWN, 'DOWN'),
+        (NOTIFICATION, 'NOTIFICATION'),
+    )
+
+    uuid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES, default=UP)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.site} - {self.type}"
--- sitecrashed/core/tests.py
+++ sitecrashed/core/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
 
-# Create your tests here.
+from sitecrashed.core.models import Site, Event
+
+
+class TestSite(TestCase):
+    def setUp(self):
+        self.site = Site.objects.create(url='https://ohmycode.com.br/')
+
+    def test_site_created(self):
+        """There should be a site."""
+        self.assertTrue(Site.objects.exists())
+
+    def test_site_quantity(self):
+        """The number of sites must be equal to 1."""
+        self.assertEqual(Site.objects.count(), 1)
+
+    def test_site_url(self):
+        """The site url should be https://ohmycode.com.br/"""
+        expected = 'https://ohmycode.com.br/'
+        self.assertEqual(self.site.url, expected)
+
+
+class TestEvent(TestCase):
+    def setUp(self) -> None:
+        self.site = Site.objects.create(url='https://ohmycode.com.br/')
+        self.event = Event.objects.create(site=self.site)
+
+    def test_event_created(self):
+        """There should be a event."""
+        self.assertTrue(Event.objects.exists())
+
+    def test_event_quantity(self):
+        """The number of events must be equal to 1."""
+        self.assertEqual(Event.objects.count(), 1)
+
+    def test_event_is_up(self):
+        """Event type should be UP"""
+        self.assertEqual(self.event.type, Event.UP)
+
+    def test_event_is_down(self):
+        """Event type should be DOWN"""
+        self.event.type = Event.DOWN
+        self.event.save()
+        self.assertEqual(self.event.type, Event.DOWN)
+
+    def test_event_is_notification(self):
+        """Event type should be NOTIFICATION"""
+        self.event.type = Event.NOTIFICATION
+        self.event.save()
+        self.assertEqual(self.event.type, Event.NOTIFICATION)
