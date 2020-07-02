af8163e98ac2299d007bf49f5af923d319ce0045
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2020-07-02 15:21:44 -0300

    added SECURE_SSL_REDIRECT variable

--- sitecrashed/settings.py
+++ sitecrashed/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', cast=bool)
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
